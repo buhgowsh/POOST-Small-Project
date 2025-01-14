c8883745e2817af1367957ca6bdf9b895d7f2099
Josh <enderslayer322@gmail.com>
2025-01-13 20:06:05 -0500

    Created a Documentation folder to store our experiences with working on the project
    This folder will be helpful to organize our information when it comes time to make the powerpoint.

---
+++ Documentation/Development for SignUp.php and DeleteContact.php
@@ -0,0 +1,12 @@
+when making the DeleteContact.php file, I mainly followed Gabriela's working files and looked
+at the delete syntax for mySQL on w3schools.com.
+	I did not find it to be very difficult, but there was an issue that was solved by copying and pasting
+  the latter half of the commands (getRequestInfo downward) from AddContact.php.
+
+	one issue i noticed was being unsure if I should try and have more parameters in the
+	delete command than one, at first I decided to leave it at one with the only one being FirstName
+	but I double checked at a lab.
+
+SignUp.php was the same process as DeleteContact.php but instead of using a delete command in the
+Contacts table, we are using an insert command in the Users table.
+	This one was easier to complete since I had finished DeleteContact.php before making this one.

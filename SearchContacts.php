454d6c4b2155d72abb12aad30ace8ca14ddc3372
Aaron <107720212+aaronab3r@users.noreply.github.com>
2025-01-30 17:04:09 -0500

    Update SearchContacts.php

--- SearchContacts.php
+++ SearchContacts.php
@@ -1,70 +1,77 @@
 <?php
-        // Tested Search & works!
-        $inData = getRequestInfo();
+// Tested Search & works!
+$inData = getRequestInfo();
 
-        $searchResults = "";
-        $searchCount = 0;
+$searchResults = "";
+$searchCount = 0;
 
-        $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-        if ($conn->connect_error)
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+if ($conn->connect_error)
+{
+    returnWithError( $conn->connect_error );
+}
+else
+{
+    // We look for partial matches in first name, last name, email, or phone
+    // for the logged in user.
+    $stmt = $conn->prepare("SELECT * FROM Contacts 
+                            WHERE (FirstName LIKE ? OR LastName LIKE ? OR Email LIKE ? OR Phone LIKE ?) 
+                              AND UserID = ?");
+    $searchInput = "%" . $inData["searchInput"] . "%";
+    $stmt->bind_param("ssssi", $searchInput, $searchInput, $searchInput, $searchInput, $inData["UserID"]);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    while($row = $result->fetch_assoc())
+    {
+        if( $searchCount > 0 )
         {
-                returnWithError( $conn->connect_error );
+            $searchResults .= ",";
         }
-        else
-        {
-                $stmt = $conn->prepare("select * from Contacts where (FirstName like ? OR LastName like ? OR Email like ? OR Phone like ?) and UserID=?");
-                $searchInput = "%" . $inData["searchInput"] . "%";
-                $stmt->bind_param("ssssi", $searchInput, $searchInput, $searchInput, $searchInput, $inData["UserID"]); // should the last s be an i?
-                $stmt->execute();
+        $searchCount++;
 
-                $result = $stmt->get_result();
+        // Return the ContactID so we can edit/delete by ID if we want.
+        $searchResults .= '{"ContactID":"' . $row["ID"] . '",';
+        $searchResults .= '"FirstName":"' . $row["FirstName"]. '",';
+        $searchResults .= '"LastName":"' . $row["LastName"]. '",';
+        $searchResults .= '"Phone":"' . $row["Phone"]. '",';
+        $searchResults .= '"Email":"' . $row["Email"]. '"}';
+    }
 
-                while($row = $result->fetch_assoc())
-                {
-                        if( $searchCount > 0 )
-                        {
-                                $searchResults .= ",";
-                        }
-                        $searchCount++;
+    if( $searchCount == 0 )
+    {
+        returnWithError( "No Records Found" );
+    }
+    else
+    {
+        returnWithInfo( $searchResults );
+    }
 
-                        // Logic taken from Leinecker YouTube tutorial of array of json objects
-                        $searchResults .= '{"FirstName" : "' . $row["FirstName"]. '", "LastName" : "' . $row["LastName"]. '", "Phone" : "' . $row["Phone"]. '", "Email" : "' . $row["Email"]. '"}';
-                }
+    $stmt->close();
+    $conn->close();
+}
 
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
 
-                if( $searchCount == 0 )
-                {
-                        returnWithError( "No Records Found" );
-                }
-                else
-                {
-                        returnWithInfo( $searchResults );
-                }
+function sendResultInfoAsJson( $obj )
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
 
-                $stmt->close();
-                $conn->close();
-        }
-        function getRequestInfo()
-        {
-                return json_decode(file_get_contents('php://input'), true);
-        }
+function returnWithError( $err )
+{
+    $retValue = '{"ID":0,"FirstName":"","LastName":"","error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
+}
 
-        function sendResultInfoAsJson( $obj )
-        {
-                header('Content-type: application/json');
-                echo $obj;
-        }
-
-        function returnWithError( $err )
-        {
-                $retValue = '{"ID":0,"FirstName":"","LastName":"","error":"' . $err . '"}';
-                sendResultInfoAsJson( $retValue );
-        }
-
-        function returnWithInfo( $searchResults )
-        {
-                $retValue = '{"results":[' . $searchResults . '],"error":""}';
-                sendResultInfoAsJson( $retValue );
-        }
-
+function returnWithInfo( $searchResults )
+{
+    $retValue = '{"results":[' . $searchResults . '],"error":""}';
+    sendResultInfoAsJson( $retValue );
+}
 ?>
